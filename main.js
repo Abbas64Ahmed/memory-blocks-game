686a0ded476eb694d501ff48ea8d7c59478e92a2
Abbas64Ahmed <ffrree64@gmail.com>
2024-09-02 18:31:13 +0300

    releas v2

--- main.js
+++ main.js
@@ -7,123 +7,35 @@ add more blocks (to make is harder make it with js)
 let statusBar = document.querySelector(".status-bar"),
     theName = document.querySelector(".the-name span"),
     wrongTries = document.querySelector(".wrong-tries span"),
-    blockContaner = document.querySelector(".blocks-contaner"),
-    blocks = Array.from(document.querySelectorAll(".blocks-contaner .block")),
     liderbord = document.querySelector("#liderboard .players"),
-    duration = 1000;
+    blockContaner = document.querySelector(".blocks-contaner"),
+    duration = 1000,
+    blocks,
+    theDifficultyLevel,
+    imgsObj = {
+        react: "img/pic (1).png",
+        anguler: "img/pic (2).png",
+        js: "img/pic (3).png",
+        vue: "img/pic (4).png",
+        flutter: "img/pic (5).png",
+        dart: "img/pic (6).png",
+        html: "img/pic (7).png",
+        css: "img/pic (8).png", 
+        python: "img/pic (9).png", 
+        cSharp: "img/pic (10).png",
+    };
 
+
+blocks = Array.from(document.querySelectorAll(".blocks-contaner .block"))
+// console.log(blocks) // ما حتشتغل هنا لانو لسه البلوكس دي ما اتعملت و لازم تتعمل تحت بعد عمل البلكات و انا عملتها جوا فنشن 
 
 window.onload = makeliderboard()
 
 document.querySelector("#start-btn").onclick = function() {
 
-    let theUserInputName = prompt("what is your name");
-
-    document.querySelector("#game-soundtreak").play()
-
-    if (theUserInputName == "" || theUserInputName == null) {
-
-        theName.innerHTML = 'unknow'
-
-    } else {
-
-        theName.innerHTML = theUserInputName;
-    }
-
     document.querySelector(".starting-screen").remove()
 
-    showallcards()
-
-        timeEnd()
-    // setTimeout(() => {
-    //     timeEnd()
-    // }, 5000);
-}
-
-
-// make the ordered array from 0 to 
-let theArray = Array.from(Array(blocks.length).keys())
-
-makeArrayRandom(theArray)
-
-// applay the random array to flax order the mix the blocks
-blocks.forEach((block , index) => {
-    block.style.order = theArray[index]
-})
-
-
-let arr = []
-blocks.forEach(block => {
-    block.addEventListener("click", (b)=> {
-        b.currentTarget.classList.add("is-fliped")
-        arr.push(b.currentTarget)
-        stopclicking(arr)
-    })
-});
-
-function machingblocks(arr) {
-    if (arr[0].dataset.tech !== arr[1].dataset.tech) {
-        document.querySelector("#fail").play()
-        setTimeout(() => {
-            arr.forEach(e => {
-                e.classList.remove("is-fliped")
-            })
-            blockContaner.classList.remove("unclickable")
-            arr.length = 0
-            wrongTries.innerHTML = parseInt(wrongTries.innerHTML) + 1
-        }, duration);
-    } else {
-        arr.forEach(e => {
-            e.classList.add("is-matched")
-            e.classList.remove("is-fliped")
-        })
-        blockContaner.classList.remove("unclickable")
-        document.querySelector("#success").play()
-        arr.length = 0
-        addtolocalstorge()
-    }
-}
-
-function addtolocalstorge() {
-    let isCopmleted = Array.from(document.querySelectorAll(".blocks-contaner .block")).filter((e) => {
-        return e.classList.contains('is-matched');
-    })
-
-    if (blocks.length === isCopmleted.length) {
-        window.localStorage.setItem(`${theName.innerHTML}`, `${wrongTries.innerHTML}`)
-        makeliderboard()
-    }
-}
-
-function stopclicking(array) {
-    if (array.length >= 2) {
-        blockContaner.classList.add("unclickable")
-        machingblocks(arr)
-    }
-}
-
-function makeArrayRandom(array) {
-    let currant = array.length
-    let temp 
-
-    for (let i = currant; i > 0 ; i--) {
-        temp = array[i]
-        array[i] = array[Math.floor(Math.random() * array[i])];
-        array[array.indexOf(array[i])] = temp;
-    }
-}
-
-
-function showallcards() {
-    blocks.forEach((block)=> {
-        block.classList.add("is-fliped")
-    })
-
-    setTimeout(()=> {
-        blocks.forEach((block)=> {
-            block.classList.remove("is-fliped")
-        })
-    }, duration)
+    choseDifficulty()
 }
 
 function timeEnd() {
@@ -153,7 +65,168 @@ function timeEnd() {
                 timeEnd()
             })
         
-    }, 180000);
+    }, 1800000);
+}
+
+function showallcards() {
+    blocks.forEach((block)=> {
+        block.classList.add("is-fliped")
+    })
+
+    setTimeout(()=> {
+        blocks.forEach((block)=> {
+            block.classList.remove("is-fliped")
+        })
+    }, duration)
+}
+
+function addtolocalstorge() {
+    let isCopmleted = Array.from(document.querySelectorAll(".blocks-contaner .block")).filter((e) => {
+        return e.classList.contains('is-matched');
+    })
+
+    if (blocks.length === isCopmleted.length) {
+        window.localStorage.setItem(`${theName.innerHTML}`, `${wrongTries.innerHTML}`)
+        makeliderboard()
+    }
+}
+
+function machingblocks(arr) {
+    if (arr[0].dataset.tech !== arr[1].dataset.tech) {
+        document.querySelector("#fail").play()
+        setTimeout(() => {
+            arr.forEach(e => {
+                e.classList.remove("is-fliped")
+            })
+            blockContaner.classList.remove("unclickable")
+            arr.length = 0
+            wrongTries.innerHTML = parseInt(wrongTries.innerHTML) + 1
+        }, duration);
+    } else {
+        arr.forEach(e => {
+            e.classList.add("is-matched")
+            e.classList.remove("is-fliped")
+        })
+        blockContaner.classList.remove("unclickable")
+        document.querySelector("#success").play()
+        arr.length = 0
+        addtolocalstorge()
+    }
+}
+
+function stopclicking(array) {
+    if (array.length >= 2) {
+        blockContaner.classList.add("unclickable")
+        machingblocks(array)
+    }
+}
+
+function makeArrayRandom(array) {
+    let currant = array.length
+    let temp 
+
+    for (let i = currant; i > 0 ; i--) {
+        temp = array[i]
+        array[i] = array[Math.floor(Math.random() * array[i])];
+        array[array.indexOf(array[i])] = temp;
+    }
+}
+
+function starting() {
+    let theUserInputName = prompt("what is your name");
+
+    document.querySelector("#game-soundtreak").play()
+
+    if (theUserInputName == "" || theUserInputName == null) {
+
+        theName.innerHTML = 'unknow'
+
+    } else {
+
+        theName.innerHTML = theUserInputName;
+    }
+
+    blocks = Array.from(document.querySelectorAll(".blocks-contaner .block"))
+
+
+    // make the ordered array from 0 to 
+    let theArray = Array.from(Array(Object.keys(imgsObj).length * (theDifficultyLevel + 1)).keys())
+
+    makeArrayRandom(theArray)
+    // applay the random array to flax order the mix the blocks
+    blocks.forEach((block , index) => {
+        block.style.order = theArray[index]
+    })
+
+
+    let arr = []
+    blocks.forEach(block => {
+        block.addEventListener("click", (b)=> {
+            b.currentTarget.classList.add("is-fliped")
+            arr.push(b.currentTarget)
+            stopclicking(arr)
+        })
+    });
+
+    showallcards()
+
+    timeEnd()
+
+}
+
+
+function makeImgFromObj(n) {
+
+    Object.keys(imgsObj).forEach((img) => {
+        for (let i = 0; i <= n; i++) {
+            let div = document.createElement("div")
+            div.classList.add("block")
+            div.setAttribute("data-tech", `${img}`)
+
+            let fornt = document.createElement("div")
+            fornt.classList.add("fornt")
+            div.appendChild(fornt)
+
+            let back = document.createElement("div")
+            back.classList.add("back")
+            div.appendChild(back)
+
+            let cardImg = document.createAttribute("img")
+            cardImg.src = `${imgsObj[img]}`
+            // back.appendChild(cardImg)
+            back.innerHTML = `<img src="${imgsObj[img]}" ></img>`
+
+            document.querySelector(".blocks-contaner").appendChild(div)
+        }
+    })
+    starting()
+}
+
+function choseDifficulty() {
+    let difficultyScreen = document.querySelector(".difficulty-screen")
+    let theLevels = document.querySelectorAll(".difficulty-screen span")
+
+    difficultyScreen.style.display = "flex"
+
+    theLevels.forEach((level) => {
+        level.addEventListener("click", (e) => {
+            if (e.target.classList.contains("easy")) {
+                theDifficultyLevel = 1
+                difficultyScreen.remove()
+                makeImgFromObj(theDifficultyLevel)
+            }
+            else if (e.target.classList.contains("normal")) {
+                theDifficultyLevel = 3
+                difficultyScreen.remove()
+                makeImgFromObj(theDifficultyLevel)
+            }
+            else if (e.target.classList.contains("hard")) {
+                theDifficultyLevel = 5
+                difficultyScreen.remove()
+                makeImgFromObj(theDifficultyLevel)
+            }
+        })
+    })
 }
 
 function makeliderboard() {
